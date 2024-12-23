6d7e1afdec7632e0d33b4c1ef4fd6615f811f998
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-23 15:37:07 +0100

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -123,6 +123,15 @@
                           <option>Kleiner Waffenschein</option>
                           <option>Großer & Kleiner Waffenschein</option>
                         </select>
+                        <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text">Upload</span>
+                      </div>
+                    </div>
                       </div>
                       </div>
                       <div class="form-group row">

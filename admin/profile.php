8f80660b5924b8fa558fac7293f424044e02b137
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-23 16:30:05 +0100

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -491,7 +491,10 @@
                       <div class="form-group row">
                         <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
                         <div class="col-sm-10">
-                          <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                        <label class="form-check-label" for="fasi_baller">Fasi PerSchutz Limo</label>
+                        <label class="form-check-label" for="fasi_baller">Fasi PerSchutz Limo</label>
+                        <label class="form-check-label" for="fasi_baller">Fasi PerSchutz Limo</label>
+
                         </div>
                       </div>
                       <div class="form-group row">

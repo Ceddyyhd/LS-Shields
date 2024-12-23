7f20762566eee41df145c18191eebbbda0d6b89a
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-23 19:54:52 +0100

    f

--- admin/include/upload_document.php
+++ admin/include/upload_document.php
@@ -1,10 +1,6 @@
 <?php
 // Datenbankverbindung einbinden
 include 'db.php';
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-header('Content-Type: application/json'); // JSON-Antwort erzwingen
 
 // Überprüfen, ob das Formular abgeschickt wurde
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {

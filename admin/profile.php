9d07f80421fb9fd603a3b9cf9568745c2c7817b4
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-24 00:44:55 +0100

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -105,7 +105,7 @@ $permissions = $stmt_permissions->fetchAll(PDO::FETCH_ASSOC);
                 </h3>
                 <p class="text-muted text-center">
                   <?php echo htmlspecialchars($user['role_name']); ?>
-                  <i class="fa-solid fa-pen-to-square"></i> 
+                  <i class="fa-solid fa-user-pen"></i> 
                 </a>
                 </p>
                 

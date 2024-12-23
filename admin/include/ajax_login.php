6e34713cdd17b7ba986fb60c42eead0d16177cff
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-23 20:06:44 +0100

    Update ajax_login.php

--- admin/include/ajax_login.php
+++ admin/include/ajax_login.php
@@ -4,6 +4,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
+$_SESSION['username'] = $username; // Setze den Benutzernamen
+
 include 'db.php';
 
 header('Content-Type: application/json');

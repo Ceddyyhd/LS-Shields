9a540af2dc466510d3ed31a6b66b58334ffcde1b
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-23 15:57:37 +0100

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -198,8 +198,20 @@
                       <div class="form-group row">
                         <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>
                         <div class="col-sm-10">
-                          <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
                         </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="inputSkills" class="col-sm-2 col-form-label">Erstehilfeschein</label>
+                        <div class="col-sm-10">
+                        <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text">Upload</span>
+                      </div>
+                    </div>                           </div>
                       </div>
                       <div class="form-group row">
                         <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>

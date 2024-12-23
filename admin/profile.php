1cf451864afcb5a686d605941b4abc2accb0ed89
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-23 16:05:10 +0100

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -277,9 +277,6 @@
                             Switzerland is small and neutral!
                             We are more like Germany, ambitious and misunderstood!
                           </div>
-                          <div class="timeline-footer">
-                            <a href="#" class="btn btn-warning btn-flat btn-sm">View comment</a>
-                          </div>
                         </div>
                       </div>
                       <!-- END timeline item -->
@@ -299,6 +296,29 @@
                     </div>
                   </div>
                   <!-- /.tab-pane -->
+
+                  <div class="modal fade" id="modal-default">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h4 class="modal-title">Default Modal</h4>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <p>One fine body&hellip;</p>
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                          <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                      </div>
+                      <!-- /.modal-content -->
+                    </div>
+                    <!-- /.modal-dialog -->
+                  </div>
+
 
                   <div class="tab-pane" id="settings">
                     <form class="form-horizontal">

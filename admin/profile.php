01143a92ef99af9b3c915bb223328df6f4091898
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-24 00:48:36 +0100

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -105,7 +105,7 @@ $permissions = $stmt_permissions->fetchAll(PDO::FETCH_ASSOC);
                 </h3>
                 <p class="text-muted text-center">
                   <?php echo htmlspecialchars($user['role_name']); ?>
-                  <button type="button" class="btn btn-block btn-default">Default</button>
+                  <button type="button" class="btn btn-primary btn-block"><i class="fa fa-bell"></i> .btn-block</button>
 
                 </a>
                 </p>

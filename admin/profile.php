14649ea2f7c5d39ff0010a3ec7b99e9a61c1b495
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-23 16:33:57 +0100

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -488,49 +488,6 @@
                         <i class="far fa-star" data-value="5"></i>
                     </div>
                 </div>
-                      <div class="form-group row">
-                        <label for="inputEmail" class="col-sm-2 col-form-label">Schließfach</label>
-                        <div class="col-sm-10">
-                        <div class="form-check">
-                          <input class="form-check-input" type="checkbox">
-                          <label class="form-check-label">Schließfach</label>
-                          <input class="form-check-input" type="checkbox" id="fasi_baller">
-                          <label class="form-check-label" for="fasi_baller">Fasi PerSchutz Limo</label>
-                        </div>
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <label for="inputName2" class="col-sm-2 col-form-label">Name</label>
-                        <div class="col-sm-10">
-                          <input type="text" class="form-control" id="inputName2" placeholder="Name">
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <label for="inputExperience" class="col-sm-2 col-form-label">Experience</label>
-                        <div class="col-sm-10">
-                          <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>
-                        <div class="col-sm-10">
-                          <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <div class="offset-sm-2 col-sm-10">
-                          <div class="checkbox">
-                            <label>
-                              <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
-                            </label>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <div class="offset-sm-2 col-sm-10">
-                          <button type="submit" class="btn btn-danger">Submit</button>
-                        </div>
-                      </div>
                     </form>
                   </div>
                   <!-- /.tab-pane -->

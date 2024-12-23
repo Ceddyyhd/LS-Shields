a9f74a4ca9bdc1050a3115d35b89381ad6bce83e
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-23 19:12:51 +0100

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -198,7 +198,27 @@ $permissions = $stmt_permissions->fetchAll(PDO::FETCH_ASSOC);
             </div>
         </div>
     </form>
-
+    <div class="modal fade" id="modal-primary">
+        <div class="modal-dialog">
+          <div class="modal-content bg-primary">
+            <div class="modal-header">
+              <h4 class="modal-title">Primary Modal</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>One fine body&hellip;</p>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-outline-light" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-outline-light">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
     <!-- Liste der hochgeladenen Dokumente -->
     <div class="mt-4">
         <h5>Hochgeladene Dokumente:</h5>

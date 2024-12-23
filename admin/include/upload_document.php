c76675df3a3791fbd27c5f383e08841b048fbb8f
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-23 19:53:42 +0100

    Update upload_document.php

--- admin/include/upload_document.php
+++ admin/include/upload_document.php
@@ -1,6 +1,10 @@
 <?php
 // Datenbankverbindung einbinden
 include 'db.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+header('Content-Type: application/json'); // JSON-Antwort erzwingen
 
 // Überprüfen, ob das Formular abgeschickt wurde
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {

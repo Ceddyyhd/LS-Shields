c08742e2ba738a1f870632ec8a220d314d72e74e
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-23 20:41:18 +0100

    Update add_note.php

--- admin/include/add_note.php
+++ admin/include/add_note.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 include 'db.php';
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {

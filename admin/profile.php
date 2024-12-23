2228a0ef2322b7a141e9e3eebd56102731d791c4
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-23 16:46:14 +0100

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -365,61 +365,41 @@
 
                       <div class="form-check mb-3">
                         <input class="form-check-input" type="checkbox" id="ortskentnisse">
-                        <label class="form-check-label" for="ortskentnisse">Ortskenntnisse</label>
+                        <label class="form-check-label" for="ortskentnisse">Bretta</label>
                       </div>
 
                       <div class="form-check mb-3">
                         <input class="form-check-input" type="checkbox" id="eventlead">
-                        <label class="form-check-label" for="eventlead">Eventlead</label>
-                      </div>
-
-                      <div class="form-check mb-3">
-                        <input class="form-check-input" type="checkbox" id="ausbilderschein">
-                        <label class="form-check-label" for="ausbilderschein">Ausbilderschein</label>
-                      </div>
-
-                      <div class="form-check mb-3">
-                        <input class="form-check-input" type="checkbox" id="fasi_baller">
-                        <label class="form-check-label" for="fasi_baller">Fasi Baller</label>
-                      </div>
-
-                      <div class="form-check mb-3">
-                        <input class="form-check-input" type="checkbox" id="eh_schulung">
-                        <label class="form-check-label" for="eh_schulung">EH-Schulung</label>
+                        <label class="form-check-label" for="eventlead">Taschenlampe</label>
                       </div>
                     </div>
                   </div>
                   <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Spezial Ausrüstung</label>
+                    <label class="col-sm-2 col-form-label">Kleidung Taktisch</label>
                     <div class="col-sm-10">
                       <div class="form-check mb-3">
                         <input class="form-check-input" type="checkbox" id="leitstelle">
-                        <label class="form-check-label" for="leitstelle">Weste</label>
+                        <label class="form-check-label" for="leitstelle">H.I.M Shark mit H II S</label>
                       </div>
 
                       <div class="form-check mb-3">
                         <input class="form-check-input" type="checkbox" id="ortskentnisse">
-                        <label class="form-check-label" for="ortskentnisse">Ortskenntnisse</label>
+                        <label class="form-check-label" for="ortskentnisse">Cargo Hose 1 S</label>
                       </div>
 
                       <div class="form-check mb-3">
                         <input class="form-check-input" type="checkbox" id="eventlead">
-                        <label class="form-check-label" for="eventlead">Eventlead</label>
+                        <label class="form-check-label" for="eventlead">Einsatzstiefel</label>
                       </div>
 
                       <div class="form-check mb-3">
                         <input class="form-check-input" type="checkbox" id="ausbilderschein">
-                        <label class="form-check-label" for="ausbilderschein">Ausbilderschein</label>
+                        <label class="form-check-label" for="ausbilderschein">Steve Stops</label>
                       </div>
 
                       <div class="form-check mb-3">
                         <input class="form-check-input" type="checkbox" id="fasi_baller">
-                        <label class="form-check-label" for="fasi_baller">Fasi Baller</label>
-                      </div>
-
-                      <div class="form-check mb-3">
-                        <input class="form-check-input" type="checkbox" id="eh_schulung">
-                        <label class="form-check-label" for="eh_schulung">EH-Schulung</label>
+                        <label class="form-check-label" for="fasi_baller">Pali Tuch</label>
                       </div>
                     </div>
                   </div>

0a55c8f6c37527c902550440dbc3a71bbf064d0d
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-23 02:54:30 +0100

    Update rangverwaltung.php

--- admin/rangverwaltung.php
+++ admin/rangverwaltung.php
@@ -166,6 +166,45 @@ $(document).ready(function () {
   </div>
 </div>
 
+<div class="modal fade" id="modal-default">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Rolle bearbeiten</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form id="editRoleForm">
+          <div class="card-body">
+            <div class="form-group">
+              <label for="roleName">Rangname</label>
+              <input type="text" id="roleName" class="form-control">
+            </div>
+            <div class="form-group">
+              <label for="roleLevel">Rang Ebene</label>
+              <select id="roleLevel" class="custom-select">
+                <option value="Inhaber">Inhaber</option>
+                <option value="Geschäftsführung">Geschäftsführung</option>
+                <option value="Mitarbeiter">Mitarbeiter</option>
+              </select>
+            </div>
+            <div class="form-group" id="permissionsContainer">
+              <!-- Dynamische Rechte erscheinen hier -->
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer justify-content-between">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Schließen</button>
+        <button type="button" class="btn btn-primary" id="saveEditRoleButton">Speichern</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <script>
  $('#saveRoleButton').click(function () {
     const name = $('#roleName').val();

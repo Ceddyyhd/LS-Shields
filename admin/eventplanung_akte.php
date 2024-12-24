34ca7916fe5b0c6c28b9f039cce0e2e3edaa17ad
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 00:35:38 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -6,6 +6,8 @@
 <div class="wrapper">
   <!-- Navbar -->
   <?php include 'include/navbar.php'; ?>
+  <!-- summernote -->
+  <link rel="stylesheet" href="../../plugins/summernote/summernote-bs4.min.css">
 
   <!-- /.navbar -->
 
@@ -169,38 +171,30 @@
                   <div class="active tab-pane" id="plan-bearbeiten">
                     
 
-                  <div class="card card-outline card-info">
-                    <div class="card-header">
-                    <h3 class="card-title">
-                        Summernote
-                    </h3>
+                  <div class="row">
+                    <div class="col-md-12">
+                    <div class="card card-outline card-info">
+                        <div class="card-header">
+                        <h3 class="card-title">
+                            Summernote
+                        </h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                        <textarea id="summernote">
+                            Place <em>some</em> <u>text</u> <strong>here</strong>
+                        </textarea>
+                        </div>
+                        <div class="card-footer">
+                        Visit <a href="https://github.com/summernote/summernote/">Summernote</a> documentation for more examples and information about the plugin.
+                        </div>
                     </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
-                    <textarea id="summernote">
-                        Place <em>some</em> <u>text</u> <strong>here</strong>
-                    </textarea>
                     </div>
-                    <div class="card-footer">
-                    Visit <a href="https://github.com/summernote/summernote/">Summernote</a> documentation for more examples and information about the plugin.
-                    </div>
-                </div>                    
+                    <!-- /.col-->
+                </div>                   
                 
-                <!-- jQuery -->
-                <script src="../../plugins/jquery/jquery.min.js"></script>
-                <!-- Bootstrap 4 -->
-                <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-                <!-- AdminLTE App -->
-                <script src="../../dist/js/adminlte.min.js"></script>
                 <!-- Summernote -->
                 <script src="../../plugins/summernote/summernote-bs4.min.js"></script>
-                <!-- CodeMirror -->
-                <script src="../../plugins/codemirror/codemirror.js"></script>
-                <script src="../../plugins/codemirror/mode/css/css.js"></script>
-                <script src="../../plugins/codemirror/mode/xml/xml.js"></script>
-                <script src="../../plugins/codemirror/mode/htmlmixed/htmlmixed.js"></script>
-                <!-- AdminLTE for demo purposes -->
-                <script src="../../dist/js/demo.js"></script>
                 <!-- Page specific script -->
                 <script>
                 $(function () {

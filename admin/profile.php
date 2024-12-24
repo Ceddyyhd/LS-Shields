0f8dd6a1f7caee2ae7c56453a646f452034529ae
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-24 01:33:13 +0100

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -114,8 +114,8 @@ $permissions = $stmt_permissions->fetchAll(PDO::FETCH_ASSOC);
 
                 <p class="text-muted text-center">
     <?php echo htmlspecialchars($user['role_name']); ?>
-    <button type="button" class="btn btn-primary fa-solid fa-pen-to-square" data-toggle="modal" data-target="#rang-bearbeiten" style="width: 50px; height: 30px; margin-left: 10px;">
-        <i class="fa-solid fa-pen-to-square"></i>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#rang-bearbeiten" style="width: 50px; height: 30px; margin-left: 10px;">
+        <i class="fa-solid fa-pen"></i>
     </button>
 </p>
 

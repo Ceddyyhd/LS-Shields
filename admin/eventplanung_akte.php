b59dfda787aca594d4edb997c9cf6cdc3e6a6176
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 00:34:24 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -10,13 +10,6 @@
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
-<!-- include libraries(jQuery, bootstrap) -->
-<script type="text/javascript" src="//code.jquery.com/jquery-3.6.0.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<!-- Summernote -->
-<script src="../../plugins/summernote/summernote-bs4.min.js"></script>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
@@ -193,12 +186,34 @@
                     </div>
                 </div>                    
                 
+                <!-- jQuery -->
+                <script src="../../plugins/jquery/jquery.min.js"></script>
+                <!-- Bootstrap 4 -->
+                <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+                <!-- AdminLTE App -->
+                <script src="../../dist/js/adminlte.min.js"></script>
+                <!-- Summernote -->
+                <script src="../../plugins/summernote/summernote-bs4.min.js"></script>
+                <!-- CodeMirror -->
+                <script src="../../plugins/codemirror/codemirror.js"></script>
+                <script src="../../plugins/codemirror/mode/css/css.js"></script>
+                <script src="../../plugins/codemirror/mode/xml/xml.js"></script>
+                <script src="../../plugins/codemirror/mode/htmlmixed/htmlmixed.js"></script>
+                <!-- AdminLTE for demo purposes -->
+                <script src="../../dist/js/demo.js"></script>
+                <!-- Page specific script -->
                 <script>
-                    $(function () {
-                        // Summernote
-                        $('#summernote').summernote()
-                    })
-                    </script>
+                $(function () {
+                    // Summernote
+                    $('#summernote').summernote()
+
+                    // CodeMirror
+                    CodeMirror.fromTextArea(document.getElementById("codeMirrorDemo"), {
+                    mode: "htmlmixed",
+                    theme: "monokai"
+                    });
+                })
+                </script>
 
                     
 

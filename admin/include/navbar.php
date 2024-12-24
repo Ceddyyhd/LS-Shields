371ab59861978e5664c9f1730981e2962ec88fc9
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-24 13:39:19 +0100

    Update navbar.php

--- admin/include/navbar.php
+++ admin/include/navbar.php
@@ -18,6 +18,8 @@ $user_name = $_SESSION['username'] ?? 'Gast'; // Standardwert, falls keine Sessi
         <a class="nav-link" data-widget="fullscreen" href="#" role="button">
           <i class="fas fa-expand-arrows-alt"></i>
         </a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" data-widget="fullscreen" href="#" role="button">
           <i class="fas fa-expand-arrows-alt"></i>
         </a>

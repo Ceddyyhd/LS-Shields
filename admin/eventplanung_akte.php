969742196550140989c17ad139ba6688ac54323c
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 00:43:06 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -168,27 +168,28 @@
                   </div>
 
                   <div class="active tab-pane" id="plan-bearbeiten">
-                  <div class="row">
-    <div class="col-md-12">
-        <div class="card card-outline card-info">
-            <div class="card-header">
-                <h3 class="card-title">
-                    Summernote
-                </h3>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-                <textarea id="summernote">
-                    Place <em>some</em> <u>text</u> <strong>here</strong>
-                </textarea>
-            </div>
-            <div class="card-footer">
-                Visit <a href="https://github.com/summernote/summernote/">Summernote</a> documentation for more examples and information about the plugin.
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-outline card-info">
+                <div class="card-header">
+                    <h3 class="card-title">
+                        Summernote
+                    </h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <textarea id="summernote">
+                        Place <em>some</em> <u>text</u> <strong>here</strong>
+                    </textarea>
+                </div>
+                <div class="card-footer">
+                    Visit <a href="https://github.com/summernote/summernote/">Summernote</a> documentation for more examples and information about the plugin.
+                </div>
             </div>
         </div>
+        <!-- /.col-->
     </div>
-    <!-- /.col-->
-</div>                   
+</div>
 
 <!-- Summernote -->
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
@@ -204,9 +205,6 @@ $(function () {
     });
 });
 </script>
-
-
-                  </div>
                   <!-- /.tab-pane -->
                   <div class="tab-pane" id="timeline">
                     <!-- The timeline -->

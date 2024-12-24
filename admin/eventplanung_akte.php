a03ce724db5b93a2a228bbfaad804ae261872d98
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 00:19:22 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -75,16 +75,15 @@
 
                 <div class="card-body">
                 <dl class="row">
-                  <dt class="col-sm-4">Description lists</dt>
+                  <dt class="col-sm-4">Haupteingang</dt>
                   <dd class="col-sm-8">A description list is perfect for defining terms.</dd>
-                  <dt class="col-sm-4">Euismod</dt>
+                  <dt class="col-sm-4">Nebeneingang</dt>
                   <dd class="col-sm-8">Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
-                  <dd class="col-sm-8 offset-sm-4">Donec id elit non mi porta gravida at eget metus.</dd>
-                  <dt class="col-sm-4">Malesuada porta</dt>
-                  <dd class="col-sm-8">Etiam porta sem malesuada magna mollis euismod.</dd>
-                  <dt class="col-sm-4">Felis euismod semper eget lacinia</dt>
-                  <dd class="col-sm-8">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo
-                    sit amet risus.
+                  <dt class="col-sm-4">Tür 1</dt>
+                  <dd class="col-sm-8">Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
+                  <dt class="col-sm-4">Tür 2</dt>
+                  <dd class="col-sm-8">Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
+
                   </dd>
                 </dl>
               </div>

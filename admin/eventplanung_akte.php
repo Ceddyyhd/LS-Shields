59579cec7dbeddee3fd8e50abb7e64fb71ad07b9
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 15:19:43 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -398,8 +398,7 @@ try {
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="far fa-clock"></i></span>
                             </div>
-                            <input type="text" class="form-control float-right" id="reservationtime" name="work_time_<?php echo $employee['id']; ?>" id="reservationtime<?php echo $employee['id']; ?>"
-                            value="<?php echo htmlspecialchars($employee['work_time']); ?>"/>
+                            <input type="text" class="form-control float-right" id="reservationtime">
                         </div>
                     </div>
                     </div>

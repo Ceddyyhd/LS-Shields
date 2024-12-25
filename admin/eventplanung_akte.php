dab477b88b93f48f42798c9e4ab9a7d337bdcafe
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 01:19:43 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -10,11 +10,14 @@
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"/>
 
-  <!-- Summernote CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
-  
+<link rel="stylesheet" href="/https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.ss" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+
+
+
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -39,6 +42,7 @@
       <div class="container-fluid">
         <div class="row">
           <div class="col-md-3">
+
             <!-- Profile Image -->
             <div class="card card-primary card-outline">
               <div class="card-body box-profile">
@@ -62,9 +66,83 @@
                   </li>
                 </ul>
               </div>
+              <!-- /.card-body -->
             </div>
+            <!-- /.card -->
+
+            <!-- About Me Box -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Informationen</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <strong><i class="fas fa-book mr-1"></i> Teams</strong>
+
+                <div class="card-body">
+                <dl class="row">
+                  <dt class="col-sm-4">Haupteingang</dt>
+                  <dd class="col-sm-8"> 
+                    <ul>
+                      <li>Cedric Schmidt</li>
+                      <li>John Schmidt</li>
+                    </ul>
+                </dd>
+                  <dt class="col-sm-4">Nebeneingang</dt>
+                  <dd class="col-sm-8"> 
+                    <ul>
+                      <li>Cedric Schmidt</li>
+                      <li>John Schmidt</li>
+                    </ul>
+                </dd>
+                  <dt class="col-sm-4">Tür 1</dt>
+                  <dd class="col-sm-8"> 
+                    <ul>
+                      <li>Cedric Schmidt</li>
+                      <li>John Schmidt</li>
+                    </ul>
+                </dd>
+                  <dt class="col-sm-4">Tür 2</dt>
+                  <dd class="col-sm-8"> 
+                    <ul>
+                      <li>Cedric Schmidt</li>
+                      <li>John Schmidt</li>
+                    </ul>
+                </dd>
+
+                  </dd>
+                </dl>
+              </div>
+
+                <hr>
+
+                <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
+
+                <p class="text-muted">Malibu, California</p>
+
+                <hr>
+
+                <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
+
+                <p class="text-muted">
+                  <span class="tag tag-danger">UI Design</span>
+                  <span class="tag tag-success">Coding</span>
+                  <span class="tag tag-info">Javascript</span>
+                  <span class="tag tag-warning">PHP</span>
+                  <span class="tag tag-primary">Node.js</span>
+                </p>
+
+                <hr>
+
+                <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
+
+                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
           </div>
-          
+          <!-- /.col -->
           <div class="col-md-9">
             <div class="card">
               <div class="card-header p-2">
@@ -77,35 +155,152 @@
               <div class="card-body">
                 <div class="tab-content">
                   <div class="active tab-pane" id="plan">
-                    <h1><u>Heading Of Message</u></h1>
+                    
+
+                  <h1><u>Heading Of Message</u></h1>
                     <h4><span style="font-size: 1rem;">Folgende Regeln:</span></h4>
                     <ul>
-                      <li>List item one</li>
-                      <li>List item two</li>
-                      <li>List item three</li>
-                      <li>List item four</li>
+                    <li>List item one</li>
+                    <li>List item two</li>
+                    <li>List item three</li>
+                    <li>List item four</li>
                     </ul>
                     <p>Standort Übersicht</p>
                     <p><img src="https://img.ceddyyhd2.eu/c51bef7c956b18e5d652994cb87d7584.png" alt="Image"></p>
+
+                    
+
+
                   </div>
 
-                  <div class="tab-pane" id="plan-bearbeiten">
-                    <!-- Summernote editor -->
-                    <textarea id="summernote"></textarea>
+                  <div class="active tab-pane" id="plan-bearbeiten">
+                    <textarea id="SummernoteID" name="description" class="form-control"></textarea>
+
+HIER SUMMERNOTE
+
+
+<script type="text/javascript">
+    $('#SummernoteID').summernote(){
+        height:255,
+    }
+</script>
+
                   </div>
+                  <!-- /.tab-pane -->
+                  <div class="tab-pane" id="timeline">
+                    <!-- The timeline -->
+                    <div class="timeline timeline-inverse">
+                      <!-- timeline time label -->
+                      <div class="time-label">
+                        <span class="bg-danger">
+                          10 Feb. 2014
+                        </span>
+                      </div>
+                      <!-- /.timeline-label -->
+                      <!-- timeline item -->
+                      <div>
+                        <i class="fas fa-envelope bg-primary"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="far fa-clock"></i> 12:05</span>
+
+                          <h3 class="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
+
+                          <div class="timeline-body">
+                            Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
+                            weebly ning heekya handango imeem plugg dopplr jibjab, movity
+                            jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
+                            quora plaxo ideeli hulu weebly balihoo...
+                          </div>
+                          <div class="timeline-footer">
+                            <a href="#" class="btn btn-primary btn-sm">Read more</a>
+                            <a href="#" class="btn btn-danger btn-sm">Delete</a>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- END timeline item -->
+                      <!-- timeline item -->
+                      <div>
+                        <i class="fas fa-user bg-info"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="far fa-clock"></i> 5 mins ago</span>
+
+                          <h3 class="timeline-header border-0"><a href="#">Sarah Young</a> accepted your friend request
+                          </h3>
+                        </div>
+                      </div>
+                      <!-- END timeline item -->
+                      <!-- timeline item -->
+                      <div>
+                        <i class="fas fa-comments bg-warning"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="far fa-clock"></i> 27 mins ago</span>
+
+                          <h3 class="timeline-header"><a href="#">Jay White</a> commented on your post</h3>
+
+                          <div class="timeline-body">
+                            Take me to your leader!
+                            Switzerland is small and neutral!
+                            We are more like Germany, ambitious and misunderstood!
+                          </div>
+                          <div class="timeline-footer">
+                            <a href="#" class="btn btn-warning btn-flat btn-sm">View comment</a>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- END timeline item -->
+                      <!-- timeline time label -->
+                      <div class="time-label">
+                        <span class="bg-success">
+                          3 Jan. 2014
+                        </span>
+                      </div>
+                      <!-- /.timeline-label -->
+                      <!-- timeline item -->
+                      <div>
+                        <i class="fas fa-camera bg-purple"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="far fa-clock"></i> 2 days ago</span>
+
+                          <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
+
+                          <div class="timeline-body">
+                            <img src="https://placehold.it/150x100" alt="...">
+                            <img src="https://placehold.it/150x100" alt="...">
+                            <img src="https://placehold.it/150x100" alt="...">
+                            <img src="https://placehold.it/150x100" alt="...">
+                          </div>
+                        </div>
+                      </div>
+                      <!-- END timeline item -->
+                      <div>
+                        <i class="far fa-clock bg-gray"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- /.tab-pane -->
 
                   <div class="tab-pane" id="settings">
                     <form class="form-horizontal">
                       <div class="form-group row">
                         <label for="inputName" class="col-sm-2 col-form-label">Name</label>
                         <div class="col-sm-10">
-                          <input type="text" class="form-control" id="inputName" placeholder="Name">
+                          <input type="email" class="form-control" id="inputName" placeholder="Name">
                         </div>
                       </div>
                       <div class="form-group row">
                         <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
                         <div class="col-sm-10">
                           <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="inputName2" class="col-sm-2 col-form-label">Name</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="inputName2" placeholder="Name">
                         </div>
                       </div>
                       <div class="form-group row">
@@ -122,20 +317,35 @@
                       </div>
                       <div class="form-group row">
                         <div class="offset-sm-2 col-sm-10">
+                          <div class="checkbox">
+                            <label>
+                              <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <div class="offset-sm-2 col-sm-10">
                           <button type="submit" class="btn btn-danger">Submit</button>
                         </div>
                       </div>
                     </form>
                   </div>
+                  <!-- /.tab-pane -->
                 </div>
+                <!-- /.tab-content -->
               </div><!-- /.card-body -->
             </div>
+            <!-- /.card -->
           </div>
+          <!-- /.col -->
         </div>
-      </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
     </section>
+    <!-- /.content -->
   </div>
-
+  <!-- /.content-wrapper -->
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.2.0
@@ -143,21 +353,13 @@
     <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
   </footer>
 
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
 </div>
-
-<!-- Summernote JS -->
-<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
-
-<!-- Initialize Summernote -->
-<script>
-  $(document).ready(function() {
-    $('#summernote').summernote({
-      height: 200, // Height of the editor
-      placeholder: 'Geben Sie hier Ihren Text ein...', // Placeholder text
-      tabsize: 2 // Tab size for indenting
-    });
-  });
-</script>
+<!-- ./wrapper -->
 
 <!-- jQuery -->
 <script src="plugins/jquery/jquery.min.js"></script>

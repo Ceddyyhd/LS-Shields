d105eb450769daa24815148c7121b82c2d942c5e
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 17:22:30 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -384,14 +384,13 @@ try {
 
                 <div class="form-group">
                   <label>Date and time:</label>
-                  <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
-                      <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime" name="reservationdatetime" 
-                      value="<?php echo isset($employee['reservationdatetime']) ? $employee['reservationdatetime'] : ''; ?>"/> <!-- Falls Daten vorhanden sind, diese laden -->
-                      <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">
-                          <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                      </div>
-                  </div>
-              </div>
+                    <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
+                        <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime"/>
+                        <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">
+                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        </div>
+                    </div>
+                </div>
                 <?php
             }
         } catch (PDOException $e) {

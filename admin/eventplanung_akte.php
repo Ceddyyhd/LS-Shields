eb738988c0332eb42f25d7c46c57f79c90187bdc
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 01:31:09 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -378,13 +378,9 @@
 <script>
   $(function () {
     // Summernote
-    $('#summernote').summernote()
-
-    // CodeMirror
-    CodeMirror.fromTextArea(document.getElementById("codeMirrorDemo"), {
-      mode: "htmlmixed",
-      theme: "monokai"
-    });
+    $('#summernote').summernote({
+        height:500,
+    })
   })
 </script>
 </body>

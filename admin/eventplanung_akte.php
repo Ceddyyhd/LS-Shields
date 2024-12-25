64ef27d5b20d1d66e5fadf6cec322b19202089aa
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 16:53:28 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -413,19 +413,20 @@ try {
 $(document).ready(function() {
     // Initialisiere datetimepicker für das "Maximal da bis"-Feld
     <?php foreach ($employees as $employee) { ?>
-        // Initialisiere datetimepicker für "Maximal da bis"
+        // Sicherstellen, dass der datetimepicker für "Maximal da bis" richtig funktioniert
         $('#timepicker<?php echo $employee['id']; ?>').datetimepicker({
             format: 'HH:mm', // Nur Stunden und Minuten
             useCurrent: false // Verhindert das automatische Setzen des aktuellen Datums
         });
 
-        // Initialisiere datetimepicker für das "Gearbeitete Zeit"-Feld
+        // Sicherstellen, dass der datetimepicker für "Gearbeitete Zeit" korrekt funktioniert
         $('#reservationtime<?php echo $employee['id']; ?>').datetimepicker({
             format: 'MM/DD/YYYY hh:mm A', // Format für Gearbeitete Zeit
             useCurrent: false, // Verhindert das automatische Setzen des aktuellen Datums
             stepping: 15, // Möglichkeit zur Auswahl von Minuten in 15-Minuten-Schritten
             showClear: true, // Möglichkeit, das Datum zu löschen
-            showClose: true // Möglichkeit, das Picker-Menü zu schließen
+            showClose: true, // Möglichkeit, das Picker-Menü zu schließen
+            sideBySide: true // Zeigt das Kalender- und Zeit-Panel nebeneinander an
         });
     <?php } ?>
 

78b8ab3c4d5be2309283776be4841eb3359d1e00
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 17:24:06 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -383,7 +383,17 @@ try {
 
 
                 <div class="form-group">
-                  <label>Date and time:</label>
+                  <label>Gestartet Um:</label>
+                    <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
+                        <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime"/>
+                        <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">
+                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                  <label>Gegangen Um:</label>
                     <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
                         <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime"/>
                         <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">

3159b6250ee824d6236b9098c73be656de78bbeb
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 15:16:03 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -466,21 +466,7 @@ try {
 });
 </script>
 
-<script>
-  $(document).ready(function() {
-    // Initialisiere datetimepicker für das erste "Maximal da bis"-Feld
-    <?php foreach ($employees as $employee) { ?>
-        $('#timepicker<?php echo $employee['id']; ?>').datetimepicker({
-            format: 'HH:mm'
-        });
 
-        // Initialisiere datetimepicker für das "Gearbeitete Zeit"-Feld
-        $('#reservationtime<?php echo $employee['id']; ?>').datetimepicker({
-            format: 'HH:mm'  // Verwende das gleiche Format für gearbeitete Zeit
-        });
-    <?php } ?>
-});
-</script>
 
 
                   <div class="tab-pane" id="externes-dokument1">

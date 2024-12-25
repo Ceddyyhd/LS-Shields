dba04db0e8f0f5a6a0ee508a27d3652102f6a934
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 17:32:20 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -383,9 +383,9 @@ try {
 
                 <div class="form-group">
                     <label>Gestartet Um:</label>
-                    <div class="input-group date" id="gestartetUm" data-target-input="nearest">
+                    <div class="input-group" id="gestartetUm" data-target-input="nearest">
                         <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fa fa-calendar"></i></span>
+                            <span class="input-group-text"><i class="far fa-clock"></i></span>
                         </div>
                         <input type="text" class="form-control datetimepicker-input" data-target="#gestartetUm" name="gestartet_um"/>
                         <div class="input-group-append" data-target="#gestartetUm" data-toggle="datetimepicker">

ac5d084f23ab75a5aa0cfe92a4dcfc808d2f0b80
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 15:13:33 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -377,6 +377,19 @@ try {
                                 </div>
                             </div>
                         </div>
+
+                        <div class="form-group">
+                              <label>Date and time range:</label>
+
+                              <div class="input-group">
+                                <div class="input-group-prepend">
+                                  <span class="input-group-text"><i class="far fa-clock"></i></span>
+                                </div>
+                                <input type="text" class="form-control float-right" id="reservationtime">
+                              </div>
+                              <!-- /.input group -->
+                            </div> 
+
                         <div class="form-group">
                           <label>Gearbeitete Zeit:</label>
                           <div class="input-group">

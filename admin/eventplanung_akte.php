fe51348d3475bc89b9f268d7bcfc481907cf7d06
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 17:18:06 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -383,13 +383,14 @@ try {
 
 
                 <div class="form-group">
-                          <label>Gearbeitete Zeit:</label>
-                          <div class="input-group">
-                              <div class="input-group-prepend">
-                                  <span class="input-group-text"><i class="far fa-clock"></i></span>
-                              </div>
-                              <input type="text" class="form-control float-right" id="reservationtime"  name="work_time_<?php echo $employee['id']; ?>" id="reservationtime<?php echo $employee['id']; ?>" value="<?php echo htmlspecialchars($employee['work_time']); ?>"/>                          </div>
-                      </div>
+                  <label>Date and time:</label>
+                    <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
+                        <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime"/>
+                        <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">
+                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        </div>
+                    </div>
+                </div>
                 <?php
             }
         } catch (PDOException $e) {

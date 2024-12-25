67a742af1a6a7d0326bda2ed939103e61b147418
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 01:38:26 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -189,8 +189,42 @@
 
                   <div class="tab-pane" id="settings">
                     
+                  <div class="card-header">
+                    <h3 class="card-title">Bootstrap Duallistbox</h3>
 
-                  
+                    <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                        <i class="fas fa-times"></i>
+                    </button>
+                    </div>
+                </div>
+                   <!-- /.card-header -->
+          <div class="card-body">
+            <div class="row">
+              <div class="col-12">
+                <div class="form-group">
+                  <label>Multiple</label>
+                  <select class="duallistbox" multiple="multiple">
+                    <option selected>Alabama</option>
+                    <option>Alaska</option>
+                    <option>California</option>
+                    <option>Delaware</option>
+                    <option>Tennessee</option>
+                    <option>Texas</option>
+                    <option>Washington</option>
+                  </select>
+                </div>
+                <!-- /.form-group -->
+              </div>
+              <!-- /.col -->
+            </div>
+            <!-- /.row -->
+          </div>
+
+          
                   </div>
                   <!-- /.tab-pane -->
                 </div>

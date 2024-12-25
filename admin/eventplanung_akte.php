79609306a83584a681a886ab6b5dca926165ec7f
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 21:15:10 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -165,10 +165,45 @@ try {
 
                   </dd>
                 </dl>
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-primary">
-                  Launch Primary Modal
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#teams-bearbeiten">
+                  Teams Bearbeiten
                 </button>
 
+                <div class="modal fade" id="teams-bearbeiten">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h4 class="modal-title">Default Modal</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        
+                      <div class="card-body">
+                <div class="row">
+                  <div class="col-3">
+                    <input type="text" class="form-control" placeholder=".col-3">
+                  </div>
+                  <div class="col-4">
+                    <input type="text" class="form-control" placeholder=".col-4">
+                  </div>
+                  <div class="col-5">
+                    <input type="text" class="form-control" placeholder=".col-5">
+                  </div>
+                </div>
+              </div>
+
+                      </div>
+                      <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                      </div>
+                    </div>
+                    <!-- /.modal-content -->
+                  </div>
+                  <!-- /.modal-dialog -->
+                </div>
               </div>
               </div>
               <!-- /.card-body -->

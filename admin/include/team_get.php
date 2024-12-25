a96bc969dfd778b8d7eabc24ee2eb8ece224ad86
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-26 00:10:24 +0100

    Update team_get.php

--- admin/include/team_get.php
+++ admin/include/team_get.php
@@ -7,7 +7,7 @@ if (isset($_GET['event_id'])) {
 
     try {
         // Abfrage, um die Team-Daten zu holen, und alle Mitarbeiter für jedes Team zu gruppieren
-        $query = "SELECT t.id AS team_id, t.team_name, t.area_name, t.employee_name, t.is_team_lead, t.id AS employee_id
+        $query = "SELECT t.id AS team_id, t.team_name, t.area_name, t.employee_name, t.is_team_lead
                   FROM team_assignments t
                   WHERE t.event_id = :event_id
                   ORDER BY t.team_name";
@@ -34,8 +34,7 @@ if (isset($_GET['event_id'])) {
             // Füge den Mitarbeiter zum entsprechenden Team hinzu
             $groupedTeams[$team['team_name']]['employee_names'][] = [
                 'name' => $team['employee_name'],
-                'is_team_lead' => $team['is_team_lead'],
-                'id' => $team['employee_id'], // Mitarbeiter ID
+                'is_team_lead' => $team['is_team_lead']
             ];
         }
 

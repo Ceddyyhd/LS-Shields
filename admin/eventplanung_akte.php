8069f7e9ba39854152ebdf0a51b1e13315b7e0eb
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 02:04:48 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -304,7 +304,7 @@
 
 
                   <div class="tab-pane" id="externes-dokument1">
-                  <iframe src="https://docs.google.com/spreadsheets/d/1sj5HHw9wTfsGk11x7vLID7bMAhHuTJC81lQRDoLKDhY/edit?usp=sharing"width="100%" height="750px"></iframe>                  
+                  <iframe src="https://docs.google.com/spreadsheets/d/1sj5HHw9wTfsGk11x7vLID7bMAhHuTJC81lQRDoLKDhY/edit?usp=sharing"width="100%" height="700px"></iframe>                  
                 </div>
                   <!-- /.tab-pane -->
                 </div>

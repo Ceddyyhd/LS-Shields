825548cfc966471196ab4c2a2b9d9c388c88c11e
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-25 14:52:41 +0100

    Update save_dienstplan.php

--- admin/include/save_dienstplan.php
+++ admin/include/save_dienstplan.php
@@ -19,6 +19,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 // Extrahiere die Mitarbeiter-ID aus dem Feldnamen
                 $employeeId = substr($key, 9);
 
+                // Überprüfe, ob die gearbeitete Zeit leer ist, und setze sie auf NULL, wenn ja
+                $workTime = !empty($_POST['work_time_' . $employeeId]) ? $_POST['work_time_' . $employeeId] : NULL;
+
                 // Hier kannst du die Daten in der Tabelle für den Dienstplan speichern
                 $stmt = $conn->prepare("
                     INSERT INTO dienstplan (event_id, employee_id, max_time, work_time)
@@ -27,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $stmt->bindParam(':event_id', $eventId, PDO::PARAM_INT);
                 $stmt->bindParam(':employee_id', $employeeId, PDO::PARAM_INT);
                 $stmt->bindParam(':max_time', $_POST['max_time_' . $employeeId], PDO::PARAM_STR);
-                $stmt->bindParam(':work_time', $_POST['work_time_' . $employeeId], PDO::PARAM_STR);
+                $stmt->bindParam(':work_time', $workTime, PDO::PARAM_STR);
                 $stmt->execute();
             }
         }

faf2f19a66388c3266970b7649de6ee08a6e6e1f
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-27 00:29:42 +0100

    Update training.php

--- admin/training.php
+++ admin/training.php
@@ -177,8 +177,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <script src="plugins/summernote/summernote-bs4.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <script>
   $(function () {
     // Summernote

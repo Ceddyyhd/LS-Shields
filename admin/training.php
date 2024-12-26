d195e92cd58989b8da292717746e94f58f48c94f
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-27 00:17:48 +0100

    Update training.php

--- admin/training.php
+++ admin/training.php
@@ -89,54 +89,61 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                 <div class="input-group-text"><i class="fa fa-calendar"></i></div>
                             </div>
                         </div>
-                          <script>
-  $(function () {
+                          
+                        <script>
+                        $(function () {
+                          // Summernote
+                          $('#summernote').summernote({
+                              height:500,
+                          })
+                          //Date picker
+                          $('#reservationdate').datetimepicker({
+                              format: 'L'
+                          });
 
-    //Date picker
-    $('#reservationdate').datetimepicker({
-        format: 'L'
-    });
+                          //Date and time picker
+                          $('#reservationdatetime').datetimepicker({ icons: { time: 'far fa-clock' } });
 
+                          //Date range picker
+                          $('#reservation').daterangepicker()
+                          //Date range picker with time picker
+                          $('#reservationtime').daterangepicker({
+                            timePicker: true,
+                            timePickerIncrement: 30,
+                            locale: {
+                              format: 'MM/DD/YYYY hh:mm A'
+                            }
+                          })
+                          //Date range as a button
+                          $('#daterange-btn').daterangepicker(
+                            {
+                              ranges   : {
+                                'Today'       : [moment(), moment()],
+                                'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                                'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+                                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                                'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+                                'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                              },
+                              startDate: moment().subtract(29, 'days'),
+                              endDate  : moment()
+                            },
+                            function (start, end) {
+                              $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+                            }
+                          )
 
-$('#datetimepicker').datetimepicker({
-        format: 'YYYY-MM-DD HH:mm', // Format für MySQL
-        icons: { // Hier kannst du die Icons für den Kalender und die Uhr setzen
-            time: 'fa fa-clock',
-            date: 'fa fa-calendar',
-            up: 'fa fa-arrow-up',
-            down: 'fa fa-arrow-down',
-            previous: 'fa fa-chevron-left',
-            next: 'fa fa-chevron-right'
-        }
-    });
+                          //Timepicker
+                          $('#timepicker').datetimepicker({
+                            format: 'LT'
+                          })
+                          //Bootstrap Duallistbox
+                          $('select.duallistbox').bootstrapDualListbox({
+                              moveOnSelect: false
+                          });
+                        })
+                      </script>
 
-    //Date and time picker
-    $('#reservationdatetime').datetimepicker({ icons: { time: 'far fa-clock' } });
-
-    //Date range picker
-    $('#reservation').daterangepicker()
-    //Date range picker with time picker
-    $('#reservationtime').daterangepicker({
-      timePicker: true,
-      timePickerIncrement: 30,
-      locale: {
-        format: 'MM/DD/YYYY hh:mm A'
-      }
-    })
-
-    //Timepicker
-    $('#timepicker').datetimepicker({
-      format: 'LT'
-    })
-  })
-
-
-
-
-
-
-</script>
-                        
                       </div>
                       <div class="modal-footer justify-content-between">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -234,9 +241,28 @@ $('#datetimepicker').datetimepicker({
 
 <!-- jQuery -->
 <script src="plugins/jquery/jquery.min.js"></script>
+<!-- Summernote -->
+<script src="plugins/summernote/summernote-bs4.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<!-- Select2 -->
+<script src="plugins/select2/js/select2.full.min.js"></script>
+<!-- Bootstrap4 Duallistbox -->
+<script src="plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+<!-- InputMask -->
+<script src="plugins/moment/moment.min.js"></script>
+<script src="plugins/inputmask/jquery.inputmask.min.js"></script>
+<!-- date-range-picker -->
+<script src="plugins/daterangepicker/daterangepicker.js"></script>
+<!-- bootstrap color picker -->
+<script src="plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Bootstrap Switch -->
+<script src="plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+<!-- BS-Stepper -->
+<script src="plugins/bs-stepper/js/bs-stepper.min.js"></script>
+<!-- dropzonejs -->
+<script src="plugins/dropzone/min/dropzone.min.js"></script>
 </body>
 </html>

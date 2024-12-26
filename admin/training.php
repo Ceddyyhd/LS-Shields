2419c0d6e6c918bad2b515a5fff1da244ad9a699
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-27 00:42:43 +0100

    Update training.php

--- admin/training.php
+++ admin/training.php
@@ -49,64 +49,84 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <!-- Main content -->
     
-    <div class="modal fade" id="modal-training-erstellen">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title">Training Erstellen</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="form-group">
-                    <label for="trainingGrund">Grund</label>
-                    <input type="text" class="form-control" id="trainingGrund" placeholder="Enter Grund">
-                </div>
-                <div class="form-group">
-                    <label for="trainingInfo">Info</label>
-                    <input type="text" class="form-control" id="trainingInfo" placeholder="Enter Info">
-                </div>
-                <div class="form-group">
-                    <label for="trainingLeitung">Trainingsleitung</label>
-                    <input type="text" class="form-control" id="trainingLeitung" placeholder="Enter Leitung">
-                </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-training-erstellen">
+                  Training Erstellen
+                </button>      
+                
+                
+                <div class="modal fade" id="modal-training-erstellen">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h4 class="modal-title">Training Erstellen</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        <div class="form-group">
+                            <label for="trainingGrund">Grund</label>
+                            <input type="text" class="form-control" id="trainingGrund" placeholder="Enter Grund">
+                        </div>
+                        <div class="form-group">
+                            <label for="trainingInfo">Info</label>
+                            <input type="text" class="form-control" id="trainingInfo" placeholder="Enter Info">
+                        </div>
+                        <div class="form-group">
+                            <label for="trainingLeitung">Trainingsleitung</label>
+                            <input type="text" class="form-control" id="trainingLeitung" placeholder="Enter Leitung">
+                        </div>
 
-                <label>Date and time:</label>
-                <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
-                    <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime" id="trainingDate"/>
-                    <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">
-                        <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        <label>Date and time:</label>
+                        <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
+                            <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime" id="trainingDate"/>
+                            <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">
+                                <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                            </div>
+                        </div>
                     </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" id="saveTraining">Save changes</button>
+            </div>
+                    </div>
+                    <!-- /.modal-content -->
+                  </div>
+                  <!-- /.modal-dialog -->
                 </div>
-            </div>
-            <div class="modal-footer justify-content-between">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" id="saveTraining">Save changes</button>
-            </div>
+
+
+          </div>
+          <!-- ./card-header -->
+          <div class="card-body">
+          <table class="table table-bordered table-hover" id="trainingTable">
+              <thead>
+                  <tr>
+                      <th>#</th>
+                      <th>Datum & Uhrzeit</th>
+                      <th>Grund</th>
+                      <th>Trainingsleitung</th>
+                      <th>Info</th>
+                      <th>An/Abmeldung</th>
+                  </tr>
+              </thead>
+              <tbody id="trainingList">
+                  <!-- Dynamische Inhalte -->
+              </tbody>
+          </table>
+          </div>
+          <!-- /.card-body -->
         </div>
+        <!-- /.card -->
+      </div>
     </div>
-</div>
-
-<div class="card-body">
-    <table class="table table-bordered table-hover" id="trainingTable">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Datum & Uhrzeit</th>
-                <th>Grund</th>
-                <th>Trainingsleitung</th>
-                <th>Info</th>
-                <th>An/Abmeldung</th>
-            </tr>
-        </thead>
-        <tbody id="trainingList">
-            <!-- Dynamische Inhalte -->
-        </tbody>
-    </table>
-</div>
-
-<script>
+    
+    
+    <script>
 // Trainings speichern
 document.getElementById('saveTraining').addEventListener('click', function() {
     var grund = document.getElementById('trainingGrund').value;
@@ -198,9 +218,6 @@ $(document).ready(function() {
     loadTrainings();
 });
 </script>
-    
-    
-    
 
       
 

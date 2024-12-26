6434c8571420b78bfea0bc8766b56dd917a46af4
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-27 00:50:44 +0100

    Update training.php

--- admin/training.php
+++ admin/training.php
@@ -274,6 +274,13 @@ $(document).ready(function() {
 <script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <script>
   $(function () {
+    //Date picker
+    $('#reservationdate').datetimepicker({
+        format: 'L'
+    });
+
+    //Date and time picker
+    $('#reservationdatetime').datetimepicker({ icons: { time: 'far fa-clock' } });
 
     //Date range picker
     $('#reservation').daterangepicker()
@@ -285,24 +292,6 @@ $(document).ready(function() {
         format: 'YYYY-MM-DD HH:mm:ss'
       }
     })
-    //Date range as a button
-    $('#daterange-btn').daterangepicker(
-      {
-        ranges   : {
-          'Today'       : [moment(), moment()],
-          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        },
-        startDate: moment().subtract(29, 'days'),
-        endDate  : moment()
-      },
-      function (start, end) {
-        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-      }
-    )
 
     //Timepicker
     $('#timepicker').datetimepicker({

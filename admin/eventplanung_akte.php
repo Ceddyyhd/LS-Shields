4c39dd24875cb8b5d2dc9f6d5330541f62436333
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-26 14:10:30 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -118,7 +118,33 @@ try {
                     <b>Ort:</b> <a class="float-right">15.12.2024</a>
                   </li>
                 </ul>
-                <button type="button" class="btn btn-block btn-primary">Primary</button>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ansprechpartner-bearbeiten">
+                  Bearbeiten
+                </button>
+
+                <div class="modal fade" id="ansprechpartner-bearbeiten">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Default Modal</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>One fine body&hellip;</p>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+
+
               </div>
               <!-- /.card-body -->
             </div>

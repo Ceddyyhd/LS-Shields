2b25feb4550b2c37bb83313df2d0c902f7a7ce59
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-26 14:47:47 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -163,12 +163,13 @@ try {
     <input type="text" class="form-control" name="telefonnummer" id="telefonnummer" required>
   </div>
 
-  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="far fa-clock"></i></span>
+  <label>Datum & Uhrzeit:</label>
+  <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
+                        <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime"/>
+                        <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">
+                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        </div>
                     </div>
-                    <input type="text" class="form-control float-right" id="reservationtime">
-                  </div>
 
   <div class="form-group">
     <label>Ort</label>

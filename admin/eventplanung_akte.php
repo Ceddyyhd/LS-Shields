d3cf9e251c2ee05b39d8d446e85d219b54d00599
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-26 15:07:21 +0100

    Update eventplanung_akte.php

--- admin/eventplanung_akte.php
+++ admin/eventplanung_akte.php
@@ -123,9 +123,6 @@ try {
             <div class="card card-primary card-outline">
               <div class="card-body box-profile">
                 <div class="text-center">
-                  <img class="profile-user-img img-fluid img-circle"
-                       src="dist/img/user4-128x128.jpg"
-                       alt="User profile picture">
                 </div>
                 <p class="text-muted text-center">Ansprechpartner</p>
                 <h3 class="profile-username text-center"><?= htmlspecialchars($event['vorname_nachname']); ?></h3>

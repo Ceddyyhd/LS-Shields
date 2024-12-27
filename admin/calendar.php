f9ff229922385b485a5eff040ec9729199f73624
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-27 22:54:42 +0100

    Update calendar.php

--- admin/calendar.php
+++ admin/calendar.php
@@ -2,6 +2,16 @@
 <html lang="en">
 <?php include 'include/header.php'; ?>
 
+<head>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+  <!-- fullCalendar -->
+  <link rel="stylesheet" href="../plugins/fullcalendar/main.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+</head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
 

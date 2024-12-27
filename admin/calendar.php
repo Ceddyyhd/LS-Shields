4b5a16ffd3cb330ed0bea066014be3a12269125e
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-27 23:12:42 +0100

    Update calendar.php

--- admin/calendar.php
+++ admin/calendar.php
@@ -71,7 +71,35 @@ while ($vacation = $stmt->fetch(PDO::FETCH_ASSOC)) {
               <!-- /.card -->
               <div class="card">
                 <div class="card-header">
-                  <h3 class="card-title">Create Event</h3>
+                <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default">
+                  Launch Default Modal
+                </button>
+
+
+                <div class="modal fade" id="modal-primary">
+        <div class="modal-dialog">
+          <div class="modal-content bg-primary">
+            <div class="modal-header">
+              <h4 class="modal-title">Primary Modal</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>One fine body&hellip;</p>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-outline-light" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-outline-light">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+
+
+      
                 </div>
                 <div class="card-body">
                   <!-- /btn-group -->

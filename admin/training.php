469e4870a0eedcf008162dc32bbd449dd784b682
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-27 02:14:33 +0100

    Update training.php

--- admin/training.php
+++ admin/training.php
@@ -47,64 +47,93 @@ scratch. This page gets rid of all links and provides the needed markup only.
     </div>
     <!-- /.content-header -->
 
-     <!-- Main content -->
+    <!-- Main content -->
     
-     <div class="row">
+    <div class="row">
       <div class="col-12">
         <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">Expandable Table</h3>
+        <?php if (isset($_SESSION['permissions']['create_trainings']) && $_SESSION['permissions']['create_trainings']): ?>
+    <div class="card-header">
+        <h3 class="card-title">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-user-create">
+                Benutzer erstellen
+            </button>
+        </h3>
+    </div>
+<?php endif; ?> 
+                
+                
+                <div class="modal fade" id="modal-training-erstellen">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h4 class="modal-title">Training Erstellen</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        <div class="form-group">
+                            <label for="trainingGrund">Grund</label>
+                            <input type="text" class="form-control" id="trainingGrund" placeholder="Enter Grund">
+                        </div>
+                        <div class="form-group">
+                            <label for="trainingInfo">Info</label>
+                            <input type="text" class="form-control" id="trainingInfo" placeholder="Enter Info">
+                        </div>
+                        <div class="form-group">
+                            <label for="trainingLeitung">Trainingsleitung</label>
+                            <input type="text" class="form-control" id="trainingLeitung" placeholder="Enter Leitung">
+                        </div>
+
+                        <label>Date and time:</label>
+                        <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
+                          <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime" id="trainingDate"/>
+                          <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">
+                              <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                          </div>
+                      </div>
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" id="saveTraining">Save changes</button>
+            </div>
+                    </div>
+                    <!-- /.modal-content -->
+                  </div>
+                  <!-- /.modal-dialog -->
+                </div>
+
+
           </div>
           <!-- ./card-header -->
           <div class="card-body">
-            <table class="table table-bordered table-hover">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Datum & Uhrzeit</th>
-                  <th>Grund</th>
-                  <th>Trainingsleitung</th>
-                  <th>Info</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr data-widget="expandable-table" aria-expanded="false">
-                  <td>1</td>
-                  <td>08.01.2024 19:00</td>
-                  <td>Szenario Training</td>
-                  <td>Aiden Knox, Paul Garcia</td>
-                  <td>10 Minuten vor Start am Firmengelände in Dienstkleidung 
-                    Genügend Essen und Trinken einpacken</td>
-                </tr>
-                <tr class="expandable-body">
-                  <td colspan="5">
-                    <div class="p-3">
-                      <div class="mb-3">
-                        <strong>Ansprechpartner:</strong>
-                        <div>Name: Tom Meyer</div>
-                        <div>Tel. Nr.: 123456789</div>
-                      </div>
-                      <div class="mb-3">
-                        <strong>Eingetragene Mitarbeiter:</strong>
-                        <ul class="mb-0">
-                          <li>Cedric Schmidt</li>
-                          <li>Falco Hunter</li>
-                        </ul>
-                      </div>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <!-- /.card-body -->
-        </div>
-        <!-- /.card -->
+    <table class="table table-bordered table-hover" id="trainingTable">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Datum & Uhrzeit</th>
+                <th>Grund</th>
+                <th>Trainingsleitung</th>
+                <th>Info</th>
+                <th>An/Abmeldung</th>
+                <?php if (isset($_SESSION['permissions']['remove_trainings']) && $_SESSION['permissions']['remove_trainings']): ?>
+                <th>Löschen</th>
+            <?php endif; ?>
+            </tr>
+        </thead>
+        <tbody id="trainingList">
+            <!-- Dynamische Inhalte -->
+        </tbody>
+    </table>
+</div>
+<!-- /.card-body -->
+</div>
+<!-- /.card -->
 </div>
 </div>
 
 <!-- Unsichtbares div mit data-username -->
-<div id="user-info" data-username="<?php echo htmlspecialchars($_SESSION['user_name']); ?>" style="display:none;"></div>
 
 <script>
 $(document).ready(function() {

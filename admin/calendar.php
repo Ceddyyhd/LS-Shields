0ac16bfec93bcf172b13924153db6c8977cad1ab
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-27 22:56:00 +0100

    Update calendar.php

--- admin/calendar.php
+++ admin/calendar.php
@@ -3,8 +3,12 @@
 <?php include 'include/header.php'; ?>
 
 <head>
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Calendar</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
   <!-- fullCalendar -->

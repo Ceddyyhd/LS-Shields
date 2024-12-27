84ac5ce83d23689494f5422dceaa820db9f7be1e
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-27 22:41:46 +0100

    Update verbesserungen.php

--- admin/verbesserungen.php
+++ admin/verbesserungen.php
@@ -178,8 +178,8 @@ include 'include/db.php';
                             </div>
                             <div class="mb-3" id="buttons-<?= $vorschlag['id'] ?>">
                             <?php if ($vorschlag['status'] === 'Eingetroffen' && ($_SESSION['permissions']['change_to_in_bearbeitung_verbesserungen'] ?? false)): ?>
-                                <button class="btn btn-block btn-outline-warning" onclick="changeStatus(<?= $vorschlag['id'] ?>, 'change_status_verbesserungen')">in Bearbeitung</button>
-                            <?php elseif ($vorschlag['status'] === 'in Bearbeitung' && ($_SESSION['permissions']['change_to_in_planung'] ?? false)): ?>
+                                <button class="btn btn-block btn-outline-warning" onclick="changeStatus(<?= $vorschlag['id'] ?>, 'change_status')">in Bearbeitung</button>
+                            <?php elseif ($vorschlag['status'] === 'in Bearbeitung' && ($_SESSION['permissions']['change_to_in_planung_verbesserungen'] ?? false)): ?>
                                 <button class="btn btn-block btn-outline-info btn-lg" onclick="changeStatus(<?= $vorschlag['id'] ?>, 'move_to_eventplanung')">Abgeschlossen</button>
                             <?php elseif ($vorschlag['status'] === 'Abgeschlossen'): ?>
                                 <!-- Kein Button für den Status 'Abgeschlossen' -->

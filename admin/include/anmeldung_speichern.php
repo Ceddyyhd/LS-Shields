62b08a07ced4f5c3fd33c1573c29be6b21992bb9
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-27 20:50:24 +0100

    Update anmeldung_speichern.php

--- admin/include/anmeldung_speichern.php
+++ admin/include/anmeldung_speichern.php
@@ -7,6 +7,7 @@ error_reporting(E_ALL);
 if (isset($_POST['event_id']) && isset($_POST['employees'])) {
     $eventId = $_POST['event_id'];
     $employeeIds = $_POST['employees'];
+    $notiz = $_POST['InputNotiz'];
 
     // Überprüfen, ob $employeeIds ein Array ist
     if (!is_array($employeeIds)) {
@@ -22,11 +23,12 @@ if (isset($_POST['event_id']) && isset($_POST['employees'])) {
         include('db.php');
 
         // Alle ausgewählten Mitarbeiter für das Event in die event_mitarbeiter_anmeldung-Tabelle eintragen
-        $stmt = $conn->prepare("INSERT INTO event_mitarbeiter_anmeldung (event_id, employee_id) VALUES (:event_id, :employee_id)");
+        $stmt = $conn->prepare("INSERT INTO event_mitarbeiter_anmeldung (event_id, employee_id, notizen) VALUES (:event_id, :employee_id, :notizen)");
 
         foreach ($employeeIds as $employeeId) {
             $stmt->bindParam(':event_id', $eventId, PDO::PARAM_INT);
             $stmt->bindParam(':employee_id', $employeeId, PDO::PARAM_INT);
+            $stmt->bindParam(':notizen', $notiz, PDO::PARAM_INT);
             $stmt->execute();
         }
 

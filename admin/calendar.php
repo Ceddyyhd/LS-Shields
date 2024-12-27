5d683d435dfbf3d07d4da976920c91c79632292e
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-27 23:11:58 +0100

    Update calendar.php

--- admin/calendar.php
+++ admin/calendar.php
@@ -74,24 +74,8 @@ while ($vacation = $stmt->fetch(PDO::FETCH_ASSOC)) {
                   <h3 class="card-title">Create Event</h3>
                 </div>
                 <div class="card-body">
-                  <div class="btn-group" style="width: 100%; margin-bottom: 10px;">
-                    <ul class="fc-color-picker" id="color-chooser">
-                      <li><a class="text-primary" href="#"><i class="fas fa-square"></i></a></li>
-                      <li><a class="text-warning" href="#"><i class="fas fa-square"></i></a></li>
-                      <li><a class="text-success" href="#"><i class="fas fa-square"></i></a></li>
-                      <li><a class="text-danger" href="#"><i class="fas fa-square"></i></a></li>
-                      <li><a class="text-muted" href="#"><i class="fas fa-square"></i></a></li>
-                    </ul>
-                  </div>
                   <!-- /btn-group -->
-                  <div class="input-group">
-                    <input id="new-event" type="text" class="form-control" placeholder="Event Title">
-
-                    <div class="input-group-append">
-                      <button id="add-new-event" type="button" class="btn btn-primary">Add</button>
-                    </div>
-                    <!-- /btn-group -->
-                  </div>
+                  
                   <!-- /input-group -->
                 </div>
               </div>

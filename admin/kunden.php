c13a161c1fa4e5607bf0f9aba1ced6cf2fa7e42c
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-29 16:04:43 +0100

    Update kunden.php

--- admin/kunden.php
+++ admin/kunden.php
@@ -206,12 +206,15 @@ $anfragen = $stmt->fetchAll(PDO::FETCH_ASSOC);
               
             <div class="row">
                   <div class="col-3">
+                    <p>Test</p>
                     <input type="text" class="form-control" placeholder=".col-3">
                   </div>
                   <div class="col-4">
+                  <p>Test2</p>
                     <input type="text" class="form-control" placeholder=".col-4">
                   </div>
                   <div class="col-5">
+                  <p>Test3</p>
                     <input type="text" class="form-control" placeholder=".col-5">
                   </div>
                 </div>

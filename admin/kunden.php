85ff1e571eb2e092cbe6d0fa5abf7e9f696143a4
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-29 18:35:45 +0100

    Update kunden.php

--- admin/kunden.php
+++ admin/kunden.php
@@ -156,28 +156,7 @@ if (!$kunde) {
         </div>
     </div>
 </div>
-<div class="mt-4">
-    <h5>Hochgeladene Dokumente:</h5>
-    <?php
-    // Dokumente aus der Datenbank abrufen
-    $stmt = $conn->prepare("SELECT file_name, file_path, uploaded_at FROM customer_documents WHERE customer_id = :customer_id");
-    $stmt->execute(['customer_id' => $customer_id]);
-    $documents = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    if (!empty($documents)): ?>
-        <ul>
-            <?php foreach ($documents as $doc): ?>
-                <li>
-                    <a href="<?= htmlspecialchars($doc['file_path']); ?>" target="_blank">
-                        <?= htmlspecialchars($doc['file_name']); ?>
-                    </a> (<?= htmlspecialchars($doc['uploaded_at']); ?>)
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    <?php else: ?>
-        <p>Keine Dokumente vorhanden.</p>
-    <?php endif; ?>
-</div>
+
 <script>
     document.getElementById("documentFile").addEventListener("change", function() {
         var fileName = this.files[0].name;

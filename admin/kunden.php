e514e9f33416985c2d20ae7a18ec258157c687f9
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-30 03:26:07 +0100

    Update kunden.php

--- admin/kunden.php
+++ admin/kunden.php
@@ -298,7 +298,7 @@ $notes = $stmt_notes->fetchAll(PDO::FETCH_ASSOC);
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title">
-                    <?php if ($_SESSION['permissions']['upload_file'] ?? false): ?>
+                    <?php if ($_SESSION['permissions']['upload_file1'] ?? false): ?>
                         Datei hochladen
                     <?php else: ?>
                         Keine Berechtigung
@@ -309,7 +309,7 @@ $notes = $stmt_notes->fetchAll(PDO::FETCH_ASSOC);
                 </button>
             </div>
 
-            <?php if ($_SESSION['permissions']['upload_file'] ?? false): ?>
+            <?php if ($_SESSION['permissions']['upload_file1'] ?? false): ?>
                 <!-- Formular zum Hochladen von Dokumenten -->
                     <form id="uploadForm" action="include/upload_document_customer.php" method="POST" enctype="multipart/form-data">
                         <!-- Kunden-ID -->

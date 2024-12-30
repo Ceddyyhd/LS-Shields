76c6761ec5ac78b3e358cae610f26d5f84cffd4a
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-30 02:20:01 +0100

    Update kundenverwaltung.php

--- admin/kundenverwaltung.php
+++ admin/kundenverwaltung.php
@@ -52,10 +52,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <thead>
           <tr>
             <th>Mitarbeiter</th>
-            <th>Rang</th>
             <th>Telefonnummer</th>
-            <th>Beitritt</th>
-            <th>Urlaub</th>
+            <th>Erstellt</th>
             <th>Bearbeiten</th>
           </tr>
         </thead>

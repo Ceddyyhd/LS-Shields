76c52f499504620cbc88e5e00e38be7f2f91c097
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-30 05:43:41 +0100

    Update generate_pdf.php

--- admin/generate_pdf.php
+++ admin/generate_pdf.php
@@ -63,8 +63,7 @@ $pdf->Ln(5);
 $pdf->Cell(0, 10, 'Status: ' . htmlspecialchars($invoice['status']), 0, 1);
 
 // Dateiname für das PDF
-$pdf_file = $_SERVER['DOCUMENT_ROOT'] . '/admin/invoices/invoice_' . $invoice_number . '.pdf';
-$pdf_url = '/admin/invoices/invoice_' . $invoice_number . '.pdf';
+$pdf_file = '/admin/invoices/invoice_' . $invoice_number . '.pdf';
 $pdf->Output($pdf_file, 'F');  // Speichern der Datei auf dem Server
 
 // Rückgabe des Pfads zur gespeicherten Datei

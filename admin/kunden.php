2ae92dfcabe0af4b4106950d8af7129eeac35904
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-30 03:59:54 +0100

    Update kunden.php

--- admin/kunden.php
+++ admin/kunden.php
@@ -569,56 +569,48 @@ $("#noteForm").on("submit", function (e) {
 <div class="tab-pane" id="rechnungen">
     
 
-<div class="card-body">
+            <div class="card-body">
                 <table class="table table-bordered">
                   <thead>
                     <tr>
                       <th style="width: 10px">#</th>
-                      <th>Task</th>
-                      <th>Progress</th>
-                      <th style="width: 40px">Label</th>
+                      <th>Rechnung</th>
+                      <th>Status</th>
+                      <th style="width: 40px">Link</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
                       <td>1.</td>
-                      <td>Update software</td>
+                      <td>Rechnung #13745</td>
                       <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                        </div>
+                      <span class="badge badge-success">Bezahlt</span>
                       </td>
-                      <td><span class="badge bg-danger">55%</span></td>
+                      <td>link zu rechnung?id=13745</td>
                     </tr>
                     <tr>
-                      <td>2.</td>
-                      <td>Clean database</td>
+                     <td>1.</td>
+                      <td>Rechnung #13745</td>
                       <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar bg-warning" style="width: 70%"></div>
-                        </div>
+                      <span class="badge badge-success">Bezahlt</span>
                       </td>
-                      <td><span class="badge bg-warning">70%</span></td>
+                      <td>link zu rechnung?id=13745</td>
                     </tr>
                     <tr>
-                      <td>3.</td>
-                      <td>Cron job running</td>
+                     <td>1.</td>
+                      <td>Rechnung #13745</td>
                       <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-primary" style="width: 30%"></div>
-                        </div>
+                      <span class="badge badge-success">Bezahlt</span>
                       </td>
-                      <td><span class="badge bg-primary">30%</span></td>
+                      <td>link zu rechnung?id=13745</td>
                     </tr>
                     <tr>
-                      <td>4.</td>
-                      <td>Fix and squish bugs</td>
+                      <td>1.</td>
+                      <td>Rechnung #13745</td>
                       <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-success" style="width: 90%"></div>
-                        </div>
+                      <span class="badge badge-success">Bezahlt</span>
                       </td>
-                      <td><span class="badge bg-success">90%</span></td>
+                      <td>link zu rechnung?id=13745</td>
                     </tr>
                   </tbody>
                 </table>

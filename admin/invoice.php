be8804e0d26152e101c1abc4ba7753b700849915
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-30 04:48:27 +0100

    Update invoice.php

--- admin/invoice.php
+++ admin/invoice.php
@@ -67,6 +67,19 @@ if (!$customer) {
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'include/header.php'; ?>
+
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+  <!-- Navbar -->
+  <?php include 'include/navbar.php'; ?>
+
+  <!-- Main Sidebar Container -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -74,7 +87,7 @@ if (!$customer) {
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Invoice</h1>
+            <h1>Invoice #<?= htmlspecialchars($invoice['invoice_number']); ?></h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -97,12 +110,23 @@ if (!$customer) {
                 <div class="col-12">
                   <h4>
                     <i class="fas fa-globe"></i> LS Shields
-                    <small class="float-right"><?= htmlspecialchars($invoice['created_at']) ?></small>
+                    <small class="float-right">Date: <?= htmlspecialchars($invoice['created_at']); ?></small>
+                    <?php
+                    // Dynamisch Status-Badge basierend auf dem Status der Rechnung
+                    $status_class = '';
+                    if ($invoice['status'] == 'Offen') {
+                        $status_class = 'badge-warning';  // Offene Rechnung
+                    } elseif ($invoice['status'] == 'Überfällig') {
+                        $status_class = 'badge-danger';  // Überfällige Rechnung
+                    } elseif ($invoice['status'] == 'Bezahlt') {
+                        $status_class = 'badge-success';  // Bezahlt
+                    }
+                    ?>
                     <span class="badge <?= $status_class; ?>"><?= htmlspecialchars($invoice['status']); ?></span>
                   </h4>
                 </div>
-                <!-- /.col -->
               </div>
+
               <!-- info row -->
               <div class="row invoice-info">
                 <div class="col-sm-4 invoice-col">
@@ -115,7 +139,6 @@ if (!$customer) {
                     Email: XXX@XXX.XXX
                   </address>
                 </div>
-                <!-- /.col -->
                 <div class="col-sm-4 invoice-col">
                   To
                   <address>
@@ -125,16 +148,11 @@ if (!$customer) {
                     Email: <?= htmlspecialchars($customer['umail']); ?>
                   </address>
                 </div>
-                <!-- /.col -->
                 <div class="col-sm-4 invoice-col">
                   <b>Invoice #<?= htmlspecialchars($invoice['invoice_number']); ?></b><br>
-                  <br>
                   <b>Payment Due:</b> <?= htmlspecialchars($invoice['due_date']); ?><br>
-                  <b>Account:</b> <?= htmlspecialchars($customer['kontonummer']); ?>
                 </div>
-                <!-- /.col -->
               </div>
-              <!-- /.row -->
 
               <!-- Table row -->
               <div class="row">
@@ -142,51 +160,45 @@ if (!$customer) {
                   <table class="table table-striped">
                     <thead>
                     <tr>
-                      <th>Description</th>
-                      <th>Unit Price</th>
-                      <th>Quantity</th>
+                      <th>Beschreibung</th>
+                      <th>Stück Preis</th>
+                      <th>Anzahl</th>
                       <th>Subtotal</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <?php foreach ($invoice_items as $item): ?>
+                    <?php
+                    foreach ($invoice_items as $item): 
+                        $subtotal = $item['unit_price'] * $item['quantity'];
+                    ?>
                     <tr>
                       <td><?= htmlspecialchars($item['description']); ?></td>
                       <td><?= htmlspecialchars($item['unit_price']); ?>$</td>
                       <td><?= htmlspecialchars($item['quantity']); ?></td>
-                      <td><?= htmlspecialchars($item['unit_price'] * $item['quantity']); ?>$</td>
+                      <td><?= $subtotal; ?>$</td>
                     </tr>
                     <?php endforeach; ?>
                     </tbody>
                   </table>
                 </div>
-                <!-- /.col -->
               </div>
-              <!-- /.row -->
 
-              <div class="row" style="margin-left: 90%;">
+              <!-- Total amount row -->
+              <div class="row" style="margin-left: 85%;">
                 <div class="col-6">
-                  <p class="lead">Amount Due</p>
-
+                  <p class="lead">Amount Due <?= htmlspecialchars($invoice['due_date']); ?></p>
                   <div class="table-responsive">
                     <table class="table">
                       <tr>
                         <th>Total:</th>
-                        <td>
-                          <?php
-                          $total = 0;
-                          foreach ($invoice_items as $item) {
-                              $total += $item['unit_price'] * $item['quantity'];
-                          }
-                          echo $total . '$';
-                          ?>
-                        </td>
+                        <td><?= $total_amount; ?>$</td>
                       </tr>
                     </table>
                   </div>
                 </div>
               </div>
 
+              <!-- Print and PDF download -->
               <div class="row no-print">
                 <div class="col-12">
                   <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
@@ -195,38 +207,23 @@ if (!$customer) {
                 </div>
               </div>
             </div>
-            <!-- /.invoice -->
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </section>
-    <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->
 
+  <!-- Footer -->
   <footer class="main-footer no-print">
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.2.0
     </div>
     <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
   </footer>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
 </div>
-<!-- ./wrapper -->
 
-<!-- jQuery -->
 <script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
-
 </body>
 </html>

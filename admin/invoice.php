72c9037ef9c0db1948b80433fc370aba22d30757
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2024-12-30 04:38:07 +0100

    Update invoice.php

--- admin/invoice.php
+++ admin/invoice.php
@@ -45,8 +45,9 @@
               <div class="row">
                 <div class="col-12">
                   <h4>
-                    <i class="fas fa-globe"></i> AdminLTE, Inc.
+                    <i class="fas fa-globe"></i> LS Shields
                     <small class="float-right">Date: 2/10/2014</small>
+                    <span class="badge badge-success">Bezahlt</span>
                   </h4>
                 </div>
                 <!-- /.col -->
@@ -56,11 +57,11 @@
                 <div class="col-sm-4 invoice-col">
                   From
                   <address>
-                    <strong>Admin, Inc.</strong><br>
-                    795 Folsom Ave, Suite 600<br>
-                    San Francisco, CA 94107<br>
-                    Phone: (804) 123-5432<br>
-                    Email: info@almasaeedstudio.com
+                    <strong>LS-Shields</strong><br>
+                    Adresse<br>
+                    Los Santos<br>
+                    Phone: XXX<br>
+                    Email: XXX@XXX.XXX
                   </address>
                 </div>
                 <!-- /.col -->
@@ -92,41 +93,36 @@
                   <table class="table table-striped">
                     <thead>
                     <tr>
-                      <th>Qty</th>
-                      <th>Product</th>
-                      <th>Serial #</th>
-                      <th>Description</th>
+                      <th>Bescheibung</th>
+                      <th>Stück Anzahl</th>
+                      <th>Anzahl</th>
                       <th>Subtotal</th>
                     </tr>
                     </thead>
                     <tbody>
                     <tr>
-                      <td>1</td>
-                      <td>Call of Duty</td>
-                      <td>455-981-221</td>
-                      <td>El snort testosterone trophy driving gloves handsome</td>
-                      <td>$64.50</td>
+                      <td>Mitarbeiter Stunde</td>
+                      <td>10$</td>
+                      <td>10</td>
+                      <td>100§</td>
                     </tr>
                     <tr>
-                      <td>1</td>
-                      <td>Need for Speed IV</td>
-                      <td>247-925-726</td>
-                      <td>Wes Anderson umami biodiesel</td>
-                      <td>$50.00</td>
+                      <td>Mitarbeiter Stunde</td>
+                      <td>10$</td>
+                      <td>10</td>
+                      <td>100§</td>
                     </tr>
                     <tr>
-                      <td>1</td>
-                      <td>Monsters DVD</td>
-                      <td>735-845-642</td>
-                      <td>Terry Richardson helvetica tousled street art master</td>
-                      <td>$10.70</td>
+                      <td>Mitarbeiter Stunde</td>
+                      <td>10$</td>
+                      <td>10</td>
+                      <td>100§</td>
                     </tr>
                     <tr>
-                      <td>1</td>
-                      <td>Grown Ups Blue Ray</td>
-                      <td>422-568-642</td>
-                      <td>Tousled lomo letterpress</td>
-                      <td>$25.99</td>
+                      <td>Rabatt</td>
+                      <td></td>
+                      <td></td>
+                      <td>5%</td>
                     </tr>
                     </tbody>
                   </table>
@@ -137,37 +133,12 @@
 
               <div class="row">
                 <!-- accepted payments column -->
-                <div class="col-6">
-                  <p class="lead">Payment Methods:</p>
-                  <img src="dist/img/credit/visa.png" alt="Visa">
-                  <img src="dist/img/credit/mastercard.png" alt="Mastercard">
-                  <img src="dist/img/credit/american-express.png" alt="American Express">
-                  <img src="dist/img/credit/paypal2.png" alt="Paypal">
-
-                  <p class="text-muted well well-sm shadow-none" style="margin-top: 10px;">
-                    Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem
-                    plugg
-                    dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
-                  </p>
-                </div>
                 <!-- /.col -->
                 <div class="col-6">
                   <p class="lead">Amount Due 2/22/2014</p>
 
                   <div class="table-responsive">
                     <table class="table">
-                      <tr>
-                        <th style="width:50%">Subtotal:</th>
-                        <td>$250.30</td>
-                      </tr>
-                      <tr>
-                        <th>Tax (9.3%)</th>
-                        <td>$10.34</td>
-                      </tr>
-                      <tr>
-                        <th>Shipping:</th>
-                        <td>$5.80</td>
-                      </tr>
                       <tr>
                         <th>Total:</th>
                         <td>$265.24</td>
@@ -182,10 +153,6 @@
               <!-- this row will not appear when printing -->
               <div class="row no-print">
                 <div class="col-12">
-                  <a href="invoice-print.html" rel="noopener" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
-                  <button type="button" class="btn btn-success float-right"><i class="far fa-credit-card"></i> Submit
-                    Payment
-                  </button>
                   <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
                     <i class="fas fa-download"></i> Generate PDF
                   </button>

a339fc726619fb43af208e7646d795287d3ec306
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-01 04:37:59 +0100

    Update calendar.php

--- admin/calendar.php
+++ admin/calendar.php
@@ -85,16 +85,6 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Urlaubsübersicht</h1>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">

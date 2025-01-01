65f96bf6a9ee87fc57f428e159498a1cb876c0bf
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-01 04:30:04 +0100

    Update fahrzeugverwaltung.php

--- admin/fahrzeugverwaltung.php
+++ admin/fahrzeugverwaltung.php
@@ -30,12 +30,12 @@
     <div class="card">
     <div class="card-header">
         <h3 class="card-title">Fahrzeuge</h3>
-        <?php if ($_SESSION['permissions']['add_vehicle'] ?? false): ?>
+    </div>
+    <?php if ($_SESSION['permissions']['add_vehicle'] ?? false): ?>
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#vehicle-create">
         Fahrzeug Hinzufügen
     </button>
     <?php endif; ?>
-    </div>
     <!-- /.card-header -->
     <div class="card-body">
         <table class="table table-bordered">

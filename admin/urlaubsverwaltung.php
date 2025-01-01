dd385bc31139c74dbaf3c3398b01e20290c37872
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-01 04:51:36 +0100

    Create urlaubsverwaltung.php

---
+++ admin/urlaubsverwaltung.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+    <?php include 'include/header.php'; ?>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <?php include 'include/navbar.php'; ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Starter Page</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    
+
+
+    <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Fahrzeuge</h3>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#vehicle-create">
+                        Fahrzeug Hinzufügen
+                        </button>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>Modell</th>
+                      <th>Kennzeichen</th>
+                      <th>Standort</th>
+                      <th>Nächste Inspektion</th>
+                      <th>Button</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1.</td>
+                      <td>Chevrolet Tahoe</td>
+                      <td>LSABC1234</td>
+                      <th>Firma</th>
+                      <td><span class="badge bg-warning">14.01.2025</span></td>
+                      <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#vehicle-bearbeiten">
+                        Fahrzeug Bearbeiten
+                        </button>
+                    </td>
+                    </tr>
+                    <tr>
+                      <td>1.</td>
+                      <td>Chevrolet Tahoe</td>
+                      <td>LSABC1234</td>
+                      <th>Firma</th>
+                      <td><span class="badge bg-danger">31.12.2024</span></td>
+                      <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#vehicle-bearbeiten">
+                        Fahrzeug Bearbeiten
+                        </button>
+                    </td>
+                    </tr>
+                    <tr>
+                      <td>1.</td>
+                      <td>Chevrolet Tahoe</td>
+                      <td>LSABC1234</td>
+                      <th>Firma</th>
+                      <td><span class="badge bg-success">14.03.2025</span></td>
+                      <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#vehicle-bearbeiten">
+                        Fahrzeug Bearbeiten
+                        </button>
+                    </td>
+                    </tr>
+                    <tr>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer clearfix">
+                <ul class="pagination pagination-sm m-0 float-right">
+                  <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                </ul>
+              </div>
+            </div>
+
+
+            <div class="modal fade" id="vehicle-bearbeiten">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Default Modal</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+            <div class="form-group">
+                        <label>Modell</label>
+                        <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+
+            <div class="form-group">
+                        <label>Kennzeichen</label>
+                        <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+
+            <div class="form-group">
+                        <label>Standort</label>
+                        <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+
+            <div class="form-group">
+                        <label>Nächste Inspektion</label>
+                        <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+
+            <div class="row">
+            <div class="form-group">
+            <label>Getankt bei </label>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox">
+                        <input type="text" class="form-control" placeholder="Enter ...">
+                        </div>
+                        
+            </div>
+            </div>
+
+
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+
+
+      <div class="modal fade" id="vehicle-create">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Fahrzeug Hinzufügen</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+            <div class="form-group">
+                        <label>Modell</label>
+                        <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+
+            <div class="form-group">
+                        <label>Kennzeichen</label>
+                        <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+
+            <div class="form-group">
+                        <label>Standort</label>
+                        <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+
+            <div class="form-group">
+                        <label>Nächste Inspektion</label>
+                        <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+
+
+
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Fahrzeuge Logs</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>Task</th>
+                      <th>Progress</th>
+                      <th style="width: 40px">Label</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1.</td>
+                      <td>Update software</td>
+                      <td>
+                        <div class="progress progress-xs">
+                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-danger">55%</span></td>
+                    </tr>
+                    <tr>
+                      <td>2.</td>
+                      <td>Clean database</td>
+                      <td>
+                        <div class="progress progress-xs">
+                          <div class="progress-bar bg-warning" style="width: 70%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-warning">70%</span></td>
+                    </tr>
+                    <tr>
+                      <td>3.</td>
+                      <td>Cron job running</td>
+                      <td>
+                        <div class="progress progress-xs progress-striped active">
+                          <div class="progress-bar bg-primary" style="width: 30%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-primary">30%</span></td>
+                    </tr>
+                    <tr>
+                      <td>4.</td>
+                      <td>Fix and squish bugs</td>
+                      <td>
+                        <div class="progress progress-xs progress-striped active">
+                          <div class="progress-bar bg-success" style="width: 90%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-success">90%</span></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer clearfix">
+                <ul class="pagination pagination-sm m-0 float-right">
+                  <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                </ul>
+              </div>
+            </div>
+
+
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>

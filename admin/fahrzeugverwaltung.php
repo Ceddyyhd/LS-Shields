241081e3515369632934670e8f1a6f41b9ceaf90
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-01 02:59:00 +0100

    Update fahrzeugverwaltung.php

--- admin/fahrzeugverwaltung.php
+++ admin/fahrzeugverwaltung.php
@@ -36,7 +36,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Bordered Table</h3>
+                <h3 class="card-title">Fahrzeuge</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
@@ -44,51 +44,36 @@ scratch. This page gets rid of all links and provides the needed markup only.
                   <thead>
                     <tr>
                       <th style="width: 10px">#</th>
-                      <th>Task</th>
-                      <th>Progress</th>
-                      <th style="width: 40px">Label</th>
+                      <th>Modell</th>
+                      <th>Kennzeichen</th>
+                      <th>Standort</th>
+                      <th>Nächste Inspektion</th>
+                      <th>Button</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
                       <td>1.</td>
-                      <td>Update software</td>
-                      <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-danger">55%</span></td>
+                      <td>Chevrolet Tahoe</td>
+                      <td>LSABC1234</td>
+                      <th>Firma</th>
+                      <td><span class="badge bg-warning">14.01.2025</span></td>
                     </tr>
                     <tr>
-                      <td>2.</td>
-                      <td>Clean database</td>
-                      <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar bg-warning" style="width: 70%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-warning">70%</span></td>
+                      <td>1.</td>
+                      <td>Chevrolet Tahoe</td>
+                      <td>LSABC1234</td>
+                      <th>Firma</th>
+                      <td><span class="badge bg-danger">31.12.2024</span></td>
                     </tr>
                     <tr>
-                      <td>3.</td>
-                      <td>Cron job running</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-primary" style="width: 30%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-primary">30%</span></td>
+                      <td>1.</td>
+                      <td>Chevrolet Tahoe</td>
+                      <td>LSABC1234</td>
+                      <th>Firma</th>
+                      <td><span class="badge bg-success">14.03.2025</span></td>
                     </tr>
                     <tr>
-                      <td>4.</td>
-                      <td>Fix and squish bugs</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-success" style="width: 90%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-success">90%</span></td>
                     </tr>
                   </tbody>
                 </table>

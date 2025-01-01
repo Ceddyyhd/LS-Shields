176c81a511a8d54cb8524550ec5f5e13412c938d
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-01 03:07:57 +0100

    Update fahrzeugverwaltung.php

--- admin/fahrzeugverwaltung.php
+++ admin/fahrzeugverwaltung.php
@@ -37,6 +37,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <div class="card">
               <div class="card-header">
                 <h3 class="card-title">Fahrzeuge</h3>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#vehicle-create">
+                        Fahrzeug Hinzufügen
+                        </button>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
@@ -158,10 +161,52 @@ scratch. This page gets rid of all links and provides the needed markup only.
       </div>
 
 
+      <div class="modal fade" id="vehicle-create">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Fahrzeug Hinzufügen</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+            <div class="form-group">
+                        <label>Modell</label>
+                        <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+
+            <div class="form-group">
+                        <label>Kennzeichen</label>
+                        <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+
+            <div class="form-group">
+                        <label>Standort</label>
+                        <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+
+            <div class="form-group">
+                        <label>Nächste Inspektion</label>
+                        <input type="text" class="form-control" placeholder="Enter ...">
+            </div>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+
+
 
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Bordered Table</h3>
+                <h3 class="card-title">Fahrzeuge Logs</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">

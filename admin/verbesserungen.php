899630226f9f3d33e503f2aeda0e0c06a7aba8ca
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-02 20:18:41 +0100

    Update verbesserungen.php

--- admin/verbesserungen.php
+++ admin/verbesserungen.php
@@ -8,6 +8,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 
 <?php include 'include/navbar.php'; ?>
 <?php

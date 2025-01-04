034862d3cd776b2c8c581b6e691bee7377c49f1a
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-04 03:03:54 +0100

    Update verbesserungen.php

--- admin/verbesserungen.php
+++ admin/verbesserungen.php
@@ -175,7 +175,7 @@ $vorschlaege = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <td>Details einblenden</td>
                 </tr>
                 <tr class="expandable-body" data-id="<?= $vorschlag['id'] ?>">
-                    <td colspan="1" div style="width: 1000px">
+                    <td colspan="1" div style="width: 50%">
                         <div class="p-3">
                             <div class="mb-3">
                                 <strong>Vorschlag:</strong>

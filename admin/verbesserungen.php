49a0fccfaf25117b931ea4d60519765cf7097094
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-04 03:04:14 +0100

    Update verbesserungen.php

--- admin/verbesserungen.php
+++ admin/verbesserungen.php
@@ -175,7 +175,7 @@ $vorschlaege = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <td>Details einblenden</td>
                 </tr>
                 <tr class="expandable-body" data-id="<?= $vorschlag['id'] ?>">
-                    <td colspan="1" div style="width: 50%">
+                    <td colspan="2" div style="width: 50%">
                         <div class="p-3">
                             <div class="mb-3">
                                 <strong>Vorschlag:</strong>
@@ -228,7 +228,7 @@ $vorschlaege = $stmt->fetchAll(PDO::FETCH_ASSOC);
                       </div>
                         </div>
                     </td>
-                    <td colspan="5">
+                    <td colspan="4">
                         <div class="p-3">
                             
                             

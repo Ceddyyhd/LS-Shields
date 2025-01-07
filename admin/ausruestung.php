ecce5b576683d4abc3df80252520577b82a968d7
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-07 16:33:22 +0100

    Update ausruestung.php

--- admin/ausruestung.php
+++ admin/ausruestung.php
@@ -115,7 +115,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
         </div>
     </div>
 </div>
-
+<?php $user_name = $_POST['user_name']; // Benutzernamen vom versteckten Input
+?>
 <!-- Modal für das Bearbeiten eines Ausrüstungstyps -->
 <div class="modal fade" id="modal-ausruestung-edit">
     <div class="modal-dialog">

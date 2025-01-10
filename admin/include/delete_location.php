82f2f61c696b8116fa8860d6e1d1649ccbe90d20
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-10 18:41:06 +0100

    Update delete_location.php

--- admin/include/delete_location.php
+++ admin/include/delete_location.php
@@ -5,12 +5,17 @@ include 'db.php';
 // Überprüfen, ob die location per POST gesendet wurde
 if (isset($_POST['location'])) {
     $location = $_POST['location'];
-    var_dump($location); // Debugging: Überprüfe den Wert der location
 
     try {
-        // SQL-Abfrage zum Löschen der Einträge mit der angegebenen Location
-        $stmt = $conn->prepare("DELETE FROM deckel WHERE location = :location");
-        $stmt->bindParam(':location', $location, PDO::PARAM_STR);
+        // Wenn location "Unbekannt" ist, also NULL in der Datenbank
+        if ($location === 'Unbekannt') {
+            // Löschen der Einträge, bei denen die location NULL ist
+            $stmt = $conn->prepare("DELETE FROM deckel WHERE location IS NULL");
+        } else {
+            // Löschen der Einträge mit der angegebenen Location
+            $stmt = $conn->prepare("DELETE FROM deckel WHERE location = :location");
+            $stmt->bindParam(':location', $location, PDO::PARAM_STR);
+        }
         $stmt->execute();
 
         // Erfolgsantwort zurückgeben

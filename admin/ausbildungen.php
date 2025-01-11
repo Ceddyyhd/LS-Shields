16818931e7f0f6549b1fbd284d176c3172d7a9d4
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-11 19:22:04 +0100

    Update ausbildungen.php

--- admin/ausbildungen.php
+++ admin/ausbildungen.php
@@ -9,10 +9,6 @@ $csrf_token = $_SESSION['csrf_token'];
 ?>
 
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="en">
 <?php include 'include/header.php'; ?>
 

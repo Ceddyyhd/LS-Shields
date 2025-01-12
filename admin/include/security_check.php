ad6d834f268595c96845831f25da663cccf5f745
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-12 03:13:36 +0100

    d

--- admin/include/security_check.php
+++ admin/include/security_check.php
@@ -1,12 +1,15 @@
 <?php 
-// Überprüfen, ob die Anfrage von der richtigen Quelle kommt (ls-shields.ceddyyhd2.eu)
 $allowed_domain = 'ls-shields.ceddyyhd2.eu';
-$host = $_SERVER['HTTP_HOST']; // Holt den Host der aktuellen Anfrage
+$allowed_methods = ['GET', 'POST'];  // Hier definierst du, welche HTTP-Methoden erlaubt sind
 
-// Sicherstellen, dass die Anfrage von der erlaubten Domain kommt
-if (strpos($host, $allowed_domain) === false) {
-    // Falls die Anfrage nicht von der erlaubten Domain kommt, Weiterleitung zur Fehlerseite
+$host = $_SERVER['HTTP_HOST'];  // Holt den Host der aktuellen Anfrage
+$request_method = $_SERVER['REQUEST_METHOD'];  // Holt die HTTP-Methode der Anfrage
+
+// Sicherstellen, dass die Anfrage von der erlaubten Domain kommt und die Methode erlaubt ist
+if (strpos($host, $allowed_domain) === false || !in_array($request_method, $allowed_methods)) {
+    // Falls die Anfrage nicht von der erlaubten Domain oder Methode kommt, Weiterleitung zur Fehlerseite
     header('Location: ../error.php');
     exit;
 }
+
 ?>

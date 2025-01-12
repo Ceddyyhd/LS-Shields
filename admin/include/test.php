f1d79c6cc92810085030d0c6b2874371733d8856
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-12 12:02:38 +0100

    Update test.php

--- admin/include/test.php
+++ admin/include/test.php
@@ -1,9 +1,7 @@
 <?php
 // Prüfen, ob die Datei direkt aufgerufen wurde
-if (basename($_SERVER['SCRIPT_NAME']) == basename(__FILE__)) {
-    // Wenn die Datei direkt aufgerufen wird und nicht über ein include
-    if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], '/admin/') === false) {
-        die('Zugriff verweigert');
-    }
+if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], '/admin/') === false) {
+    // Wenn die Datei ohne die korrekte Referenz aufgerufen wurde
+    die('Zugriff verweigert');
 }
 ?>

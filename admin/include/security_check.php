f3ae1dc67e09ad45a0aec82b795f318d13188b99
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-12 13:42:57 +0100

    Update security_check.php

--- admin/include/security_check.php
+++ admin/include/security_check.php
@@ -1,1 +1,8 @@
+<?php
+// Sicherstellen, dass die Datei nicht direkt über den Browser aufgerufen wird
+if ($_SERVER['REQUEST_METHOD'] !== 'POST' && $_SERVER['REQUEST_METHOD'] !== 'GET') {
+    die('Zugriff verweigert');
+}
 
+// Dein Code folgt hier
+?>

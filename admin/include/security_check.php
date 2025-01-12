5f750e0e9fc53dcda8d6390f7d7cf12eb7722383
Ceddyyhd <77159834+Ceddyyhd@users.noreply.github.com>
2025-01-12 12:05:27 +0100

    Update security_check.php

--- admin/include/security_check.php
+++ admin/include/security_check.php
@@ -1,6 +1,6 @@
 <?php
 // Prüfen, ob die Datei direkt aufgerufen wurde
-if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], '/admin/') === false) {
+if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], '/include/') === false) {
     // Wenn die Datei ohne die korrekte Referenz aufgerufen wurde, Weiterleitung zu error.php
     header('Location: ../error.php');
     exit();
